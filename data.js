27ddf0424d4cec33d1caba6652efab0b1b4d449e
Mary Mosman <mbMosman@users.noreply.github.com>
2019-06-24 10:38:39 -0500

    Reverted data.js to staff

--- data.js
+++ data.js
@@ -1,82 +1,30 @@
 var people = [
   {
-    name: 'Abdul',
-    githubUsername: 'Abdulrismail9'
+    name: 'Dev',
+    githubUsername: 'devjanaprime'
   },
   {
-    name: 'Amie',
-    githubUsername: 'amitao'
+    name: 'Dane',
+    githubUsername: 'DoctorHowser'
   },
   {
-    name: 'Anders',
-    githubUsername: 'rydena021'
+    name: 'Mary',
+    githubUsername: 'mbmosman'
   },
   {
-    name: 'Bode',
-    githubUsername: 'bodecci'
+    name: 'Chris',
+    githubUsername: 'christopher-black'
   },
   {
-    name: 'Carson',
-    githubUsername: 'sundrata'
+    name: 'Kris',
+    githubUsername: 'kdszafranski'
   },
   {
-    name: 'David',
-    githubUsername: 'dmayou'
+    name: 'Luke',
+    githubUsername: 'LukeSchlangen'
   },
   {
-    name: 'Isaiah',
-    githubUsername: 'Buckhalton'
-  },
-  {
-    name: 'JD',
-    githubUsername: 'jdsghuman'
-  },
-  {
-    name: 'John',
-    githubUsername: 'JohnAWorley'
-  },
-  {
-    name: 'Josh',
-    githubUsername: 'J-Byron'
-  },
-  {
-    name: 'Madison',
-    githubUsername: 'mlhershkovitz'
-  },
-  {
-    name: 'Morgan',
-    githubUsername: 'morgancostigan'
-  },
-  {
-    name: 'Phia',
-    githubUsername: 'phiathao'
-  },
-  {
-    name: 'Rebecca',
-    githubUsername: 'fieldofficeco'
-  },
-  {
-    name: 'Ryan',
-    githubUsername: 'ryanmundy'
-  },
-  {
-    name: 'Said',
-    githubUsername: 'saidomar23'
-  },
-  {
-    name: 'Tang',
-    githubUsername: 'ntxooyaj89'
-  },
-  {
-    name: 'Travis',
-    githubUsername: 'travislang'
-  },
-  {
-    name: 'Vang',
-    githubUsername: 'vxiong029'
-  },
-  {
-    name:'Victoria',
-    githubUsername: 'victoriachhieng'
+    name: 'Ally',
+    githubUsername: 'Lysautumn'
   }
 ];
