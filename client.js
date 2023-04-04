1891aa648ee0aab161562f5d91695889651913a2
Luckie Bah <bah966@gmail.com>
2023-04-04 11:47:53 +0100

    added images linked css jquery and then set up css to display images correctly now doing the js

--- client.js
+++ client.js
@@ -1,1 +1,19 @@
+$(document).ready(onReady)
+
+console.log('You got this!');
 console.log('Here are all the available people:', people);
+
+function onReady(){
+    console.log('Youre doing great');
+    displayProfiles()
+}
+
+function displayProfiles(){
+    console.log('Nice getting there');
+    for (let individual of people) {
+        $('#profileDisplay').append(`
+        <span>${individual.image} <span>
+        `)
+    }
+
+}
--- data.js
+++ data.js
@@ -1,30 +1,38 @@
+
 var people = [
   {
     name: 'Dev',
-    githubUsername: 'devjanaprime'
+    githubUsername: 'devjanaprime',
+    image: `<div class="chris"><img src="/img/dev.jpeg" alt=""></div>`
   },
   {
     name: 'Dane',
-    githubUsername: 'DoctorHowser'
+    githubUsername: 'DoctorHowser',
+    image: `<div class="chris"><img src="/img/Dane.jpeg" alt=""></div>`
   },
   {
     name: 'Matt',
-    githubUsername: 'matthew-black'
+    githubUsername: 'matthew-black',
+    image: `<div class="chris"><img src="/img/mathewBlack.jpeg" alt=""></div>`
   },
   {
     name: 'Kris',
-    githubUsername: 'kdszafranski'
+    githubUsername: 'kdszafranski',
+    image: `<div class="chris"><img src="/img/Kris.jpeg" alt=""></div>`
   },
   {
     name: 'Edan',
-    githubUsername: 'eschwartz'
+    githubUsername: 'eschwartz',
+    image: `<div class="chris"><img src="/img/Edan.jpeg" alt=""></div>`,
   },
   {
     name: 'Liz',
-    githubUsername: 'emkerber'
+    githubUsername: 'emkerber',
+    image: `<div class="chris"><img src="/img/Liz.jpeg" alt=""></div>`
   },
   {
     name: 'Chris',
-    githubUsername: 'christopher-black'
+    githubUsername: 'christopher-black',
+    image: `<div class="chris"><img src="/img/chris.jpeg" alt=""></div>`
   },
 ];
